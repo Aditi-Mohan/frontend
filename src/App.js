0f8e45eb3ccf9be69a5d1efdf4b1f38149007e28
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2020-08-11 21:35:40 +0530

    Zoomable Worldmap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Settings from './components/Setting'
+import WorldMap from './components/WorldMap';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <NavBar/>
+      <Route exact path='/' component={WorldMap}/>
       <Route path='/settings' component={Settings}/>
       </BrowserRouter>
     </div>
