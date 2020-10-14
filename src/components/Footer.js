b50ee6d91cb606f2d558d1dc75d54a8345a969f2
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2020-10-14 20:59:22 +0530

    smol changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,9 +17,9 @@ function Footer(props) {
     }, [window.innerWidth]);
 
     return(
-        <div className='my-navbar' style={{display: 'flex', paddingLeft: '37%', height: 79, bottom: 0, width: '100%'}}>
+        <div className='my-navbar' style={{display: 'flex', paddingLeft: '40%', height: 79, bottom: 0, width: '100%'}}>
             <li data-tip data-for='github' className='nav-item' style={{float: 'left', cursor: 'pointer'}}>
-                <div className='my-icon-button'><Github/></div>
+                <a href='https://github.com/Aditi-Mohan' target='_blank'><div className='my-icon-button'><Github/></div></a>
                 <ReactStyledTooltip id='github' place='top' effect='solid'>
                     <div>Github</div>
                 </ReactStyledTooltip>
@@ -31,13 +31,13 @@ function Footer(props) {
                 </ReactStyledTooltip>
             </li>
             <li data-tip data-for='kaggle' className='nav-item' style={{float: 'left', cursor: 'pointer'}}>
-                <div className='my-icon-button'><Kaggle/></div>
+                <a href='https://www.kaggle.com/aditimon' target='_blank' ><div className='my-icon-button'><Kaggle/></div></a>
                 <ReactStyledTooltip id='kaggle' place='top' effect='solid'>
                     <div>Kaggle</div>
                 </ReactStyledTooltip>
             </li>
             <li data-tip data-for='patreon' className='nav-item' style={{float: 'left', cursor: 'pointer'}}>
-                <div className='my-icon-button'><Coffee/></div>
+                <a href='https://www.buymeacoffee.com/aditimon' target='_blank'><div className='my-icon-button'><Coffee/></div></a>
                 <ReactStyledTooltip id='patreon' place='top' effect='solid'>
                     <div>Buy Me a Coffee</div>
                 </ReactStyledTooltip>
